73947e857d09d7a5caf51fdc9015e32e9e3cecfd
Yamamoto-Keishi <yamamoto_keishi@lszzg.com>
2026-01-08 11:45:10 +0900

    設定画面修正

--- lib/features/news/ui/news_page.dart
+++ lib/features/news/ui/news_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../data/post_repository.dart';
-import '../data/settings_repository.dart';
 import '../view_model/news_page_view_model.dart';
 import 'news_page_body.dart';
 
@@ -10,19 +9,11 @@ class NewsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<SettingsRepository>(
-      future: SettingsRepository.create(),
-      builder: (context, snap) {
-        if (!snap.hasData) {
-          return const Scaffold(
-            body: Center(child: CircularProgressIndicator()),
-          );
-        }
-        return ChangeNotifierProvider(
-          create: (_) => NewsPageViewModel(context.read<PostRepository>(),)..loadInitial(),
-          child: const NewsPageBody(),
-        );
-      },
+    return ChangeNotifierProvider(
+      create: (_) => NewsPageViewModel(
+        context.read<PostRepository>(),
+      )..loadInitial(),
+      child: const NewsPageBody(),
     );
   }
 }
